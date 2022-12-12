c752966bdb4832968bee763b156e9e1e449baee7
yojeongjin <osolku@naver.com>
2022-12-13 01:54:53 +0900

    remove .git files

---
+++ back/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var db = require('./config/db');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+var conn = db.init()
+db.conn(conn)
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../front/build')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
---
+++ back/routes/api/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.all('*',(req, res)=>{
+	res.status(404).send({success:false, msg:`api unknown uri ${req.path}`});
+})
+
+module.exports = router;
