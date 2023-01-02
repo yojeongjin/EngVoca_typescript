c6d10e9dc0ae1039bc7afd68c631e7afeede88f7
yojeongjin <osolku@naver.com>
2023-01-02 22:50:00 +0900

    button nav 디자인 수정 location.href 별로 디자인을 다르게 주어 사용자에게 현재 어느페이지에 위치해있는지 알릴 수 있도록 수정함
    -

--- front/src/components/BottomNav.tsx
+++ front/src/components/BottomNav.tsx
@@ -1,18 +1,28 @@
-import React, { useState, } from 'react';
+import React, { useEffect, useState, } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import ProfileContainer from '../containers/ProfileContainer';
 import Modal from './Modal';
 
-
-
 const BottomNav: React.FC = () => {
   const [ openModal, setOpenModal ] = useState<boolean>(false)
+  const [ activeIdx, setActiveIdx ] = useState(0)
   const location = useLocation()
 
   const showModal = () => {
     setOpenModal(true)
   }
+  useEffect(() => {
+    if(location.pathname === '/') {
+      setActiveIdx(15)
+    } else if (location.pathname === '/eachtype') {
+      setActiveIdx(155)
+    } else if (location.pathname === '/repeat') {
+      setActiveIdx(225)
+    } else if (location.pathname === '/notebook') {
+      setActiveIdx(295)
+    }
+  },[])
 
   return (
     <BottomBase>
@@ -64,7 +74,12 @@ const BottomNav: React.FC = () => {
             />
           </Link>
         </BottomNavLink>
+
+        <TubeLight activeIdx={activeIdx}>
+          <Light></Light>
+        </TubeLight>
       </BottomNavList>
+
       {openModal && 
       (<Modal setOpenModal={setOpenModal}>
         <ProfileContainer />
@@ -82,15 +97,15 @@ bottom: 0;
 left: 0;
 width: 100%;
 height: 45px;
-background-color: #d9e1e8;
+background-color: #7f9eb2;
 display: flex;
 justify-content: center;
 align-items: center;
 `
 
 const BottomNavList = styled.nav`
+position: relative;
 width: 360px;
-border-top: 1px solid #ddd;
 `
 
 const BottomNavLink = styled.div<{isActive: boolean}>`
@@ -100,10 +115,33 @@ text-align: center;
 height: 45px;
 line-height: 45px;
 box-shadow: 0px 10px 5px -5px gray;
-transform: translate(${(props) =>  props.isActive ? '0, -20%' : '0, 0'});
+opacity: ${(props) =>  props.isActive ? '1' : '0.4'};
 &:hover {
   scale: 1.2;
 }
+`
+
+const TubeLight = styled.div<{activeIdx: number}>`
+position: absolute;
+left: ${(props) => props.activeIdx}px;
+top: 0px;
+width: 45px;
+height: 5px;
+border-radius: 5px;
+background: #fff;
+transition: left 400ms ease;
+z-index: 10;
+`
+
+const Light = styled.div`
+position: absolute;
+left: -30%;
+top: 5px;
+width: 160%;
+height: 80px;
+clip-path: polygon(5% 100%, 25% 0px, 75% 0px, 95% 100%);
+background: linear-gradient(to bottom, rgba(255, 255, 255, 0.3) -50%, rgba(255, 255, 255, 0) 90%);
+pointer-events: none;
 `
 
 const ImgWrap = styled.div`
